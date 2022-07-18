deea4e648b7230f08c9b20e29366ccedd2e92056
Chirag Parmar <chirag.parmar@civica2.com>
2022-07-18 16:22:13 +0530

    task 6

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ApplicationDbContext _context;
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Items;
+            return View("Index", data);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        { 
+            return View("Create");
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");  
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var item = _context.Items.Find(Id);
+            if (item != null)
+            { 
+                _context.Items.Remove(item);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
