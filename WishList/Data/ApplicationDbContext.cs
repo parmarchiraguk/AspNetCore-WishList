e6c563aa602eed1fdd7934b8015c7aae594b0456
Chirag Parmar <chirag.parmar@civica2.com>
2022-07-18 14:59:44 +0530

    task 4

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        private readonly DbContextOptions dbContextOptions;
+
+        public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions dbContextOptions)
+        {
+            this.dbContextOptions = dbContextOptions;
+        }
+    }
+}
